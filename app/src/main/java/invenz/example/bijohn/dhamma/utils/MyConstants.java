c0b0465ecae4a21a2e85fcc0acf2bf1e1c7b94be
Bijohn <roy.bijohn07@gmail.com>
2018-08-17 23:15:22 +0600

    Commit 3

--- app/src/main/java/invenz/example/bijohn/dhamma/utils/MyConstants.java
+++ app/src/main/java/invenz/example/bijohn/dhamma/utils/MyConstants.java
@@ -6,7 +6,8 @@ public class MyConstants {
     public static final String SHARED_PREF_NAME = "share_pref";
 
 
-    public static final String DEFAULT_URL = "http://192.168.43.166/dhamma/";
+    //public static final String DEFAULT_URL = "http://192.168.43.166/dhamma/";
+    public static final String DEFAULT_URL = "http://invenz-it.com/dhamma/";
     public static final String STORE_TOKEN_URL = DEFAULT_URL+"StoreToken.php";
 
 
